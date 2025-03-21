492bc1c9fd0c9a0eaecd3e66de27debafd83f88b
jh7r <gabriel.budzko@gmx.de>
2025-03-21 15:36:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America/Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '+49 17632637049'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
